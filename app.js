7e694b611d26658722205703c1eb404af07ad467
Igor Kuznetsov <apagof01@yandex.ru>
2023-06-20 11:30:48 +0300

    fixed: app

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
 const mainRouter = require('./routes');
+const errorsHandler = require('./middlewares/errorsHandler');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5fea1309760f053b4c12804c',
-  };
+app.use(cookieParser());
 
-  next();
-});
+app.use(limiter);
 
 app.use(mainRouter);
 
+app.use(errors());
+app.use(errorsHandler);
+
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
