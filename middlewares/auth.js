f7d26a2e2418d29be026861d07410d969932ddf2
Igor Kuznetsov <apagof01@yandex.ru>
2023-06-20 15:05:41 +0300

    fixed: auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,15 +2,32 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorizedError');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  let payload;
+  // const token = req.cookies.jwt;
+  // let payload;
+  // try {
+  //   if (!token) {
+  //     next(new UnauthorizedError('Необходима авторизация'));
+  //   }
+  //   payload = jwt.verify(token, 'some-secret-key');
+  // } catch (err) {
+  //   next(new UnauthorizedError('Необходима авторизация'));
+  // }
+
+  // req.user = payload;
+
+  // next();
+  let token;
   try {
-    if (!token) {
-      next(new UnauthorizedError('Необходима авторизация'));
-    }
-    // попытаемся верифицировать токен
+    token = req.cookies.jwt;
+  } catch (e) {
+    next(new UnauthorizedError('Необходима авторизация'));
+  }
+
+  let payload;
+
+  try {
     payload = jwt.verify(token, 'some-secret-key');
-  } catch (err) {
+  } catch (e) {
     next(new UnauthorizedError('Необходима авторизация'));
   }
 
