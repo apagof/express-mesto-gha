858cc559ef8941baf2ec56d91d9ae0023c6453ae
Igor Kuznetsov <apagof01@yandex.ru>
2023-06-21 00:01:44 +0300

    fixed: refError p.2

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,24 +1,9 @@
-// module.exports = (err, req, res, next) => {
-//   const { statusCode = 500, message } = err;
+module.exports = (err, req, res, next) => {
+  const { statusCode = 500, message } = err;
 
-//   res.status(statusCode).send({
-//     message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-//   });
-
-//   next();
-// };
-
-const ReferenceError = require('../errors/reference-err');
-
-const errorsHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode || ReferenceError;
-
-  const message = statusCode === ReferenceError
-    ? 'На сервере произошла ошибка'
-    : err.message;
-  res.status(statusCode).send({ message });
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+  });
 
   next();
 };
-
-module.exports = errorsHandler;
