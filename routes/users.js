3f3cf5ef31640c8d18c060e3f8e2d721395eed14
Igor Kuznetsov <apagof01@yandex.ru>
2023-06-20 12:03:02 +0300

    fixed:users_path

--- routes/users.js
+++ routes/users.js
@@ -13,24 +13,24 @@ const {
 
 router.use(cookieParser());
 
-router.get('/users', getUsers);
+router.get('/', getUsers);
 
-router.get('/users/me', getCurrentUser);
+router.get('/me', getCurrentUser);
 
-router.get('/users/:userId', celebrate({
+router.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).hex().required(),
   }),
 }), getUserById);
 
-router.patch('/users/me', celebrate({
+router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), updateProfile);
 
-router.patch('/users/me/avatar', celebrate({
+router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     // eslint-disable-next-line no-useless-escape
     avatar: Joi.string().regex(LINK_REGULAR),
